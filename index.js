56605d8237e5a61eb4a25572e35a9b4f49a3bca8
Natalie <89657349+harr1003@users.noreply.github.com>
2023-02-17 12:56:42 -0500

    Created index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+$('#screenwidth').text(window.screen.width);
+$('#screenheight').text(window.screen.height);
+$('#windowwidth').text($(window).width());
+$('#windowheight').text($(window).height());
