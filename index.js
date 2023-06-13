e81ee2f77cbb44ad1d79b7b916a4237826a02150
Natalie <89657349+harr1003@users.noreply.github.com>
2023-06-13 13:45:53 -0400

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-$('#screenwidth').text(window.screen.width);
-$('#screenheight').text(window.screen.height);
-$('#windowwidth').text($(window).width());
-$('#windowheight').text($(window).height());
